3f5a59b42a8e67ae18cb723197715e6b98ee3004
pawel.bak <pawel.bak@vao.pl>
2019-11-22 15:50:49 +0100

    promises

--- app/appTedious.js
+++ app/appTedious.js
@@ -1,56 +1,81 @@
-const dbConfig = require('./config.js');
-const RecipientFinance = require('../utils/RecipientFinance');
-const config = require('../utils/config');
+const config = require('./config');
+const Connection = require('tedious').Connection;
+const Request = require('tedious').Request;
+const TYPES = require('tedious').TYPES;
+const async = require('async');
+const promisify = require('util').promisify;
 
-var express = require('express');
-var app = express();
-app.get('/', function (req, res) {
-  connectionTest()
-});
-app.listen(5000, () => console.log('Serwer running'));
+const createConnectionPromise = promisify(createConnection);
+const closeConnectionPromise = promisify(closeConnection);
+const executeRequestPromise = promisify(executeRequest);
 
-const recipientFinance = new RecipientFinance(config, 0)
+async function test() {
+  const parents = [1,2,3];
+  const indexs = [1,2,3,4,5,6,7,8];
 
-function connectionTest() {
-  const result = recipientFinance.send();
-  console.log(result);
-};
+  for(index of indexs) {
+    const connection = await createConnectionPromise();
+    const result = await executeRequestPromise(connection, index);
+    console.log(result);
+  }
+}
+test();
 
+function createConnection(callback) {
+  const connection = new Connection({
+    authentication: {
+      type: "default",
+      options: {
+        userName: config.username,
+        password: config.password
+      }
+    },
+    server: config.host,
+    options: {
+      port: config.port,
+      database: config.database
+    }
+  });
 
-  // const connection = new Connection({
-  //   authentication: {
-  //     type: "default",
-  //     options: {
-  //       userName: config.username,
-  //       password: config.password
-  //     }
-  //   },
-  //   server: config.host,
-  //   options: {
-  //     port: config.port,
-  //     database: config.database
-  //   }
-  // });
-  //
-  // console.log(connection);
-  // const sql = 'UPDATE dbo.a3_table_vessel SET created = @created WHERE id = 30';
-  // const insertDate = new Date(`2019-10-10 08:3${index}:36.504135`);
-  // index += 1;
-  // const request = new Request(sql, function(err, data) {});
-  // request.addParameter('created', TYPES.DateTimeOffset, insertDate);
-  //
-  // request.on('requestCompleted', function (data) {
-  //   console.log('completed', data);
-  //   console.log('closing connections');
-  //   connection.close();
-  // });
-  //
-  // connection.on('connect', function(err) {
-  //   connection.execSql(request);
-  // });
-  //
-  // connection.on('end', function(err) {
-  //   console.log('connection closed');
-  //   console.log(connection);
-  // });
+  connection.on('connect', function () {
+    return callback(null, connection)
+  });
+
+  connection.on('error', function (err) {
+    return callback(err)
+  });
+}
+
+async function executeRequest(connection, index, callback) {
+  const sql = 'UPDATE dbo.a3_table_vessel SET created = @created WHERE id = 30';
+  // const sql = 'INSERT INTO dbo.a3_table_vessel (id, created) VALUES (31, @created)';
+  const insertDate = new Date(`2019-10-10 08:3${index}:36.504135`);
+  const request = new Request(sql, function(err, data) {});
+  request.addParameter('created', TYPES.DateTimeOffset, insertDate);
+
+  connection.execSql(request);
+
+  connection.on('errorMessage', async function(err) {
+    await closeConnectionPromise(connection);
+    return callback(err)
+  });
+
+  request.on('requestCompleted', async function () {
+    console.log('completed');
+    console.log('closing connections');
+    await closeConnectionPromise(connection);
+    callback(null, `result-${index}`);
+  });
+}
+
+function closeConnection(connection, callback) {
+  connection.close();
+  connection.on('end', function(err) {
+    console.log('connection closed');
+    if (err) {
+      return callback(err);
+    }
+    callback();
+  });
+}
 
